8080b30e112c82ac2708e29d2456e61f40e3508e
Chiara <chiara.emina@studio.unibo.it>
2026-02-02 16:45:25 +0100

    add method declarations to GameView

--- src/main/java/it/unibo/makeanicecream/view/GameViewImpl.java
+++ src/main/java/it/unibo/makeanicecream/view/GameViewImpl.java
@@ -1,10 +1,65 @@
 package it.unibo.makeanicecream.view;
 
+import it.unibo.makeanicecream.api.GameController;
 import it.unibo.makeanicecream.api.GameView;
 
 /**
- *
+ * A {@link DrawNumberView} implementation .
  */
 public class GameViewImpl implements GameView {
+
+    @Override
+    public void setController(GameController controller) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setController'");
+    }
+
+    @Override
+    public void showCustomer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showCustomer'");
+    }
+
+    @Override
+    public void showOrder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showOrder'");
+    }
+
+    @Override
+    public void showTimer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showTimer'");
+    }
+
+    @Override
+    public void showLives() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showLives'");
+    }
+
+    @Override
+    public void showIngredients() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showIngredients'");
+    }
+
+    @Override
+    public void showIceCream() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showIceCream'");
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
 
 }
