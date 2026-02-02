39bda04753959c8a6e9927c7184a189655e20942
Chiara <chiara.emina@studio.unibo.it>
2026-02-02 11:00:56 +0100

    add method declarations to GameController interface

--- src/main/java/it/unibo/makeanicecream/api/GameController.java
+++ src/main/java/it/unibo/makeanicecream/api/GameController.java
@@ -1,8 +1,35 @@
 package it.unibo.makeanicecream.api;
 
 /**
- *
+ * Implementation of the game controller.
+ * <p>
+ * This class coordinates the game execution by acting as the Control component
+ * in the MVC/ECB architecture. It receives user actions from the Boundary
+ * components (views), updates the game state accordingly, and notifies the
+ * boundaries about changes to be displayed.
+ * </p>
  */
+
 public interface GameController {
-                    
+    
+    /**
+     * Starts a new game at the specified level.
+     * 
+     * @param levelNumber the number of the level
+     */
+    void startGame(int levelNumber);
+
+    /**
+     * Handles a user action coming from the view.
+     * 
+     * @param action the user action to be processed
+     */
+    void handleInput(GameAction action);
+
+    /**
+     * Updates the game state.
+     * 
+     * @param deltaTime the time in seconds since tha last update
+     */
+    void updateGame(double deltaTime);
 }
