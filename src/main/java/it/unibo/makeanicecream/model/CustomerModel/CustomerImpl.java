c0be1ccdb934414a389aaaf0a13f5d2ce993a513
Sophy <sophy.mercuri@studio.unibo.it>
2026-02-05 18:45:06 +0100

    Creazione di CustomerImpl nel model e aggiunta di metodi nella interfaccia Customer

---
+++ src/main/java/it/unibo/makeanicecream/model/CustomerModel/CustomerImpl.java
@@ -0,0 +1,11 @@
+package it.unibo.makeanicecream.model.CustomerModel;
+
+import it.unibo.makeanicecream.api.Customer;
+import it.unibo.makeanicecream.api.Order;
+import it.unibo.makeanicecream.api.Timer;
+
+public class CustomerImpl implements Customer {
+    
+    /*
+    CREAZIONE CUSTOMER IMPL    
+}
