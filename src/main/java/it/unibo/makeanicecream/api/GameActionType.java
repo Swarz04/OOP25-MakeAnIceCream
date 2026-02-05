998ed587a0cc9090afcdd1129ed7f21b509b5bd2
Chiara <chiara.emina@studio.unibo.it>
2026-02-05 19:21:14 +0100

    add ChooseConeCommand

--- src/main/java/it/unibo/makeanicecream/api/GameActionType.java
+++ src/main/java/it/unibo/makeanicecream/api/GameActionType.java
@@ -4,6 +4,7 @@ package it.unibo.makeanicecream.api;
  * Defines the possible types of actions the user can perform in the game.
  */
 public enum GameActionType {
+    CHOOSE_CONE,
     ADD_INGREDIENT,
     DELIVER,
     CANCEL,
