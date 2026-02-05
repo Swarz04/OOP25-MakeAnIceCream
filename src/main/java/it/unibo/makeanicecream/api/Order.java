1b07fbe32b15b8a4a0468df8b2f33b9ac0d7208f
Sophy <sophy.mercuri@studio.unibo.it>
2026-02-05 22:55:34 +0100

    Redefinizione di Order

--- src/main/java/it/unibo/makeanicecream/api/Order.java
+++ src/main/java/it/unibo/makeanicecream/api/Order.java
@@ -1,9 +1,11 @@
 package it.unibo.makeanicecream.api;
 
-/**
- *
- */
+import java.util.List;
+
 public interface Order {
 
-    boolean isSatisfiedBy(Icecream icecream);       
+    List<Ingredient> getFlavors(); //Ingredient di type SCOOP
+    Ingredient getCone(); //Ingrediente di type CONE
+    List<Ingredient>getToppings(); //Ingredient con Type=LIQUID_TOPPING O SOLID_TOPPING
+    boolean isSatisfiedBy(Icecream icecream); //verifica tra ordine e gelato
 }
