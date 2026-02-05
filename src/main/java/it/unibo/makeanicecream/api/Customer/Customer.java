f79293c421444cc64b44b3f42ba772461489d73d
Sophy <sophy.mercuri@studio.unibo.it>
2026-02-05 01:56:33 +0100

    Definizione della interfaccia Costumer

--- src/main/java/it/unibo/makeanicecream/api/Customer/Customer.java
+++ src/main/java/it/unibo/makeanicecream/api/Customer/Customer.java
@@ -1,8 +1,11 @@
 package it.unibo.makeanicecream.api.Customer;
 
+import it.unibo.makeanicecream.api.Icecream;
 /**
  *
  */
 public interface Customer {
-
+    
+    String getName();
+    boolean checkIceCream(Icecream icecream);
 }
