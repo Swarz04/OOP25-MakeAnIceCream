7fb2243b19ccec28f33935af8e10ea0ff39cce10
Sophy <sophy.mercuri@studio.unibo.it>
2026-02-07 04:40:14 +0100

    Sistemazione di eventuali commit mancanti in CustomerTemplate

--- src/main/java/it/unibo/makeanicecream/model/CustomerModel/CustomerTemplate.java
+++ src/main/java/it/unibo/makeanicecream/model/CustomerModel/CustomerTemplate.java
@@ -12,7 +12,15 @@ public class CustomerTemplate {
     private final int toppingCount;
     private int nameCounter = 0;
 
-    
+    /**
+     * 
+     * Constructs a CustomerTemplate with the specified configuration.
+     * Note: Time is NOT included here as it's determined by the game level
+     * 
+     * @param names possible names for costumers of this template
+     * @param scoops number of flavor scoops required
+     * @param toppings number of toppings required
+     */
     public CustomerTemplate(String[] names, int scoops, int toppings){
 
     }
